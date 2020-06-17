f6272c528bdbdbd1bdd2bfb53a2c9210dadc3a23
Salman-Raza-stack <salmanabid301@gmail.com>
2020-06-18 04:56:32 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,8 +19,8 @@ function App() {
         href = "https://reactjs.org"
         target = "_blank"
         rel = "noopener noreferrer" >
-        Hello World <
-        /a> < /
+        Hello World. < /a> < /
+        a > < /
         header > <
         /div>
     );
