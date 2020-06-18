2e76e984d6b0febafffc1f9ed106b9932466fe38
Salman-Raza-stack <salmanabid301@gmail.com>
2020-06-18 05:17:25 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-    return ( 
-    <div className ="App">
-        <h1>Hello World</h1>
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        Hello world from Salman
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learning React From Panacloud
+      </a>
+      </header>
     </div>
-    );
+  );
 }
 
 export default App;
