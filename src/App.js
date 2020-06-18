95f7fef7959c0bb693e4b5d571e3715c5177f1dd
Salman-Raza-stack <salmanabid301@gmail.com>
2020-06-18 05:25:17 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        Hello world from waqas
+        Hello world from Salman
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learning React With Panacloud
         </a>
       </header>
     </div>
