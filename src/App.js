f7cb852ae7fa98ad81971f66e1d7caf70679da55
Salman-Raza-stack <salmanabid301@gmail.com>
2020-06-20 02:28:04 +0500

    class 3 start

--- src/App.js
+++ src/App.js
@@ -3,18 +3,7 @@ import "./App.css";
 import Dinner from "./dinner.js";
 
 function App() {
-  return (
-    <div className="App">
-      <h1> Hello From Salman </h1>
-      <Dinner dishName="Biryani" sweets="Kheer" />
-      <br />
-      <hr />
-      <Dinner dishName="Polaow" sweets="Gajar ka Halwa" />
-      <hr />
-      <br />
-      <Dinner dishName="Karhai" sweets="Lambah Sheri" />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
--- src/dinner.js
+++ src/dinner.js
@@ -2,10 +2,7 @@ import React from "react";
 
 function Dinner(props) {
   return (
-    <div>
-      <h1>Today We are not serving Chicken {props.dishName}</h1>
-      <h1>Today We are not serving Chicken {props.sweets}</h1>
-    </div>
+    
   );
 }
 
