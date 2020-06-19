60e64638821ce0271e8f86c3a5efa23b5e60e881
Salman-Raza-stack <salmanabid301@gmail.com>
2020-06-20 03:06:00 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import "./App.css";
 function App() {
   // Variables to store componenet data
 
-  let [count, setCount] = useState(0);
+  let [count, setCount] = useState(1);
 
   return (
     <div>
       <h1>Value of the counter is : {count}</h1>
       <br />
-      <button onClick={() => setCount(2)}>Update Counter</button>
+      <button onClick={() => setCount(++count)}>Update Counter</button>
     </div>
   );
 }
