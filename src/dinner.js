0edb7e90fb0376f890563f35f8b292775d33b646
Salman-Raza-stack <salmanabid301@gmail.com>
2020-06-19 05:47:27 +0500

    Update dinner.js

--- src/dinner.js
+++ src/dinner.js
@@ -3,7 +3,7 @@ import React from "react";
 function Dinner(props) {
   return (
     <div>
-      <h1>Today We are serving Chicken Biryani {props.Biryani}</h1>
+      <h1>Today We are serving Chicken {props.Biryani}</h1>
     </div>
   );
 }
