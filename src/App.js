e13a9bbd521a0fd4cc6ac2be4366e0f0983edcd8
Salman-Raza-stack <salmanabid301@gmail.com>
2020-06-19 05:36:28 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React from 'react';
-
+import React from "react";
+import "./App.css";
+import Dinner from "./dinner";
 
 function App() {
-    return <h1> Hello From Salman </h1>
+  return (
+    <div className="App">
+      <h1> Hello From Salman </h1>
+      <Dinner />
+    </div>
+  );
 }
 
-export default App
+export default App;
