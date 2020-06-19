f556ebb81cc495fc1d7210957be69c786cb7c288
Salman-Raza-stack <salmanabid301@gmail.com>
2020-06-20 03:40:19 +0500

    day and night

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ function App() {
   // Variables to store componenet data
 
   let [count, setCount] = useState(1);
+  let [isMorning, setMorning] = useState(true);
 
   return (
-    <div>
+    <div className="box">
+      <h2>Day time = {isMorning ? "Morning" : "Night"}</h2>
       <Message counter={count} />
       <br />
       <button onClick={() => setCount(++count)}>Update Counter</button>
