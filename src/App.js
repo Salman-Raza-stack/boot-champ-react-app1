a2ece0990ed5828b671e1e1354b65ba3884b66e2
Salman-Raza-stack <salmanabid301@gmail.com>
2020-06-20 02:51:25 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Dinner from "./dinner.js";
 
 function App() {
+  // Variables to store componenet data
+
+  let [count, setCount] = useState(0);
+
   return (
     <div>
-      <h1>Hello world</h1>
+      <h1>Value of the counter is : {count}</h1>
     </div>
   );
 }
