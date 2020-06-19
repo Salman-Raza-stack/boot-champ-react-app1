6ed9be69a22a871ec2305009a643733fdf18fb97
Salman-Raza-stack <salmanabid301@gmail.com>
2020-06-19 05:56:24 +0500

    now its okay

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
     <div className="App">
       <h1> Hello From Salman </h1>
       <Dinner dishName="Biryani" sweets="Kheer" />
+      <Dinner dishName="Polaow" sweets="Gajar ka Halwa" />
+      <Dinner dishName="Karhai" sweets="Lambah Sheri" />
     </div>
   );
 }
--- src/dinner.js
+++ src/dinner.js
@@ -3,8 +3,8 @@ import React from "react";
 function Dinner(props) {
   return (
     <div>
-      <h1>Today We are serving Chicken Biryani {props.Biryani}</h1>
-      <h1>Today We are serving Kheer as Sweets {props.Kheer}</h1>
+      <h1>Today We are serving Chicken {props.dishName}</h1>
+      <h1>Today We are serving Chicken {props.sweets}</h1>
     </div>
   );
 }
